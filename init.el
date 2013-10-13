cb6e31e4aa40373c889f1d53b9f690fbcd2c1c99
Tero Kadenius <tero.kadenius@gmail.com>
2013-10-13 18:01:06 +0300

    Pisketti pack turns desktop-save-mode on to save desktop on exit and load it from current dir on startup.

--- init.el
+++ init.el
@@ -5,3 +5,7 @@
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
+
+;; a) Automatically save desktop on exit
+;; b) Automatically load previously saved desktop from the current directory
+(desktop-save-mode 1)
