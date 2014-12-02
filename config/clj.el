a416629b45a803b9c06bafdf076e4f50d9853d51
pisketti <tero.kadenius@gmail.com>
2014-12-02 23:56:23 +0200

    Adds a utility function cljs-repl which outputs the cemerick piggiepack magic for connecting to browser repl from cider

--- config/clj.el
+++ config/clj.el
@@ -2,3 +2,8 @@
   "When changing into a namespace with in-ns in repl, clojure.core will not be loaded. This eases the typing"
   (interactive)
   (insert "(clojure.core/refer 'clojure.core)"))
+
+(defun cljs-repl ()
+  "Start nrepl (lein repl) and connect to it with cider (M-x cider). Then connect to cljs repl using cemerick.piggiepack."
+  (interactive)
+  (insert "(require 'cljs.repl.browser) (cemerick.piggieback/cljs-repl :repl-env (cljs.repl.browser/repl-env :port 9000))"))
