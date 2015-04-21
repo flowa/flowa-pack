b2a5232a4e34eea573ebd7c1f024a35b3dd5f288
pisketti <tero.kadenius@gmail.com>
2015-04-21 21:08:17 +0300

    Adds keybindings for resizing windows

--- config/bindings.el
+++ config/bindings.el
@@ -24,3 +24,14 @@
 
 ;; TODO bind mc/edit-lines to some keys. The following conflicts search
 ;;(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+
+
+;; ---------------
+;; Resize windows
+;; ---------------
+
+(global-set-key (kbd "C-å") 'enlarge-window)
+(global-set-key (kbd "C-ä") 'shrink-window)
+
+(global-set-key (kbd "C-.") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-,") 'shrink-window-horizontally)
