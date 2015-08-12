37b9c1dadc18d142f5bb33cd4ab6963a8227314f
Juha Heimonen <heimojuh@gmail.com>
2015-08-12 22:53:43 +0300

    Adds flycheck to the pack
    * Adds submodules for depencencies
    * dash
    * f
    * s
    * let-alist
    
    The let-alist was problematic, since it's included in emacs 25.
    Not sure if everything works as should.
    But it works!
    
    Also adds hooks for scripts that need to be hooked into,
    as in this case if I understood things right.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 Two steps to configure:
 * copy .emacs-live.el to your home dir
 * copy rest of the contents to ~/.emacs.d/packs/flowa-pack/
+* go to the clone directory and run:
+  ```
+  git submodule init
+  git submodule update
+  ```
--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;; Init file for pisketti pack
 
+;; Add pack libs
+(live-add-pack-lib "let-alist.el")
+(live-add-pack-lib "flycheck")
+(live-add-pack-lib "f.el")
+(live-add-pack-lib "s.el")
+(live-add-pack-lib "dash.el")
+
 ;; Load bindings config
 (live-load-config-file "bindings.el")
 
@@ -8,6 +15,9 @@
 
 ;; Clojure bells and whistles
 (live-load-config-file "clj.el")
+
+;; Load hooks
+(live-load-config-file "hooks.el")
 
 ;; a) Automatically save desktop on exit
 ;; b) Automatically load previously saved desktop from the current directory
@@ -18,5 +28,12 @@
 
 ;; Enable switching between windows with s-<arrow-keys>
 (windmove-default-keybindings)
-
 (require 'multiple-cursors)
+(require 'let-alist)
+(require 'dash)
+(require 's)
+(require 'f)
+(require 'flycheck)
+
+(provide 'init)
+;;; init.el ends here
