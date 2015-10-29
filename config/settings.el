a0e27ef476fd0d1f4dbbfe508d45d9b306854790
Juha Heimonen <heimojuh@gmail.com>
2015-10-29 09:55:05 +0200

    Store backups to tmp
    * Store backup and autosave to temp directory
    * removes interlock files.
    
    latter may cause problems when editing same file with two editors.

--- config/settings.el
+++ config/settings.el
@@ -5,4 +5,14 @@
 
 (setq live-disable-zone t)
 
+;; store all backup and autosave files in the tmp dir)
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; disable emacs interlock files (this may cause problems when editing same
+;; file with two instances of emacs. but who would do that.
+(setq create-lockfiles nil)
 ;;; settings ends here
