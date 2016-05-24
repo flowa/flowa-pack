dcdf85dc2832f46c67cc380c7623b00ca16239c4
Juha Heimonen <heimojuh@gmail.com>
2016-05-24 07:30:26 +0300

    Add info.el
    new versions of live need this

---
+++ info.el
@@ -0,0 +1,3 @@
+(live-pack-name "emacs-live-flowa-pack")
+(live-pack-version (live-version)) ;; keep in sync with Overtone Live
+(live-pack-description "Includes general goodness used in flowa company")
