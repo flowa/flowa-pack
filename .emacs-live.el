d15c10f971c7d128cc1dcd4b05bef0df8661fde1
Juha Heimonen <juha.heimonen@flowa.fi>
2018-09-23 10:41:41 +0300

    Change pack location in default emacs-live.el
    Decouples from emacs-live base directory, in order to make updating the
    core live more easy.

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,1 +1,12 @@
-(live-add-packs '(flowa-pack))
+;;; emacs-live.el --- configuration file for emacs-live
+;;; Commentary:
+
+;;; Currently loads flowa-pack outside of emacs-live, in order
+;;; to make updating emacs-live more easy.
+;;; Indeed, power of the horse!
+
+;;; Code:
+
+(live-add-packs '(~/.flowa-pack))
+
+;;; emacs-live.el ends here
